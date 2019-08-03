b3e92c931385d041f45d4170dce9fc960c248083
Min Zhe Kang <36184648+axois@users.noreply.github.com>
2019-08-03 21:13:27 +0800

    updated

--- web/main.js
+++
@@ -1,9 +0,0 @@
-function printit(){
-    var data = document.getElementById("cow").value;
-    eel.printme(data, data)(callback);
-    //python
-}
-
-function callback(ret1){
-    document.getElementById("TobePrinted").innerHTML = ret1
-}
