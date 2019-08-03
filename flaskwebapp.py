5df30d9c01931739ff14f572143ea3c9be8662ac
Kang Min Zhe <36184648+minzhekang@users.noreply.github.com>
2019-08-03 21:37:20 +0800

    Update flaskwebapp.py

--- flaskwebapp.py
+++ flaskwebapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='../python-eel-framework/web/')
+app = Flask(__name__, template_folder='python-eel-framework/web')
 
 @app.route('/')
 def index():
